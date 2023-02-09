fe9c54440961c50870ef7b46c072a4eb75fae942
vb10 <vbacik.10@gmail.com>
2023-02-10 00:24:53 +0300

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,10 +8,16 @@ import 'package:sesimiduy/core/enums/core_localize.dart';
 import 'package:sesimiduy/features/home/view/home_view.dart';
 import 'package:sesimiduy/product/app_builder.dart';
 
+import 'package:sesimiduy/firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   final coreLocalize = CoreLocalize();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     EasyLocalization(
