3f38c65f7b3aee4a4db88b6a2d757b7eb70b59ec
vb10 <vbacik.10@gmail.com>
2023-02-10 02:11:45 +0300

    added login view design

---
+++ lib/product/enum/icon_size.dart
@@ -0,0 +1,12 @@
+enum IconSize {
+  //size is [10]
+  small(10),
+  //size is [25]
+  medium(25),
+  //size is [50]
+  large(50);
+
+  final double value;
+  // ignore: sort_constructors_first
+  const IconSize(this.value);
+}
