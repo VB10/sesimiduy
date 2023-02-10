2eb8a92beff3bc01816682ef8dcc56383d0ba6be
alperenekin <alperen.ekin@zuehlke.com>
2023-02-10 23:20:14 +0100

    Fixed icon issue, created dropdown and toggle button on the map

--- lib/features/current_map/view/current_map_view.dart
+++ lib/features/current_map/view/current_map_view.dart
@@ -1,10 +1,12 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:kartal/kartal.dart';
 import 'package:sesimiduy/product/init/language/locale_keys.g.dart';
 import 'package:sesimiduy/product/items/colors_custom.dart';
 import 'package:sesimiduy/product/utility/constants/app_constants.dart';
 import 'package:sesimiduy/product/utility/constants/image_constants.dart';
+import 'package:sesimiduy/product/utility/padding/page_padding.dart';
 import 'package:sesimiduy/product/utility/size/index.dart';
 
 class CurrentMapView extends StatefulWidget {
@@ -17,17 +19,20 @@ class CurrentMapView extends StatefulWidget {
 class _CurrentMapViewState extends State<CurrentMapView> with _ByteMapHelper {
   static const _defaultLocation = LatLng(37.579609, 36.946812);
   static const _defaultLocationIST = LatLng(40.5333232, 31.0325468);
+  final List<bool> _selectedOptions = <bool>[true, false];
 
   @override
   void initState() {
     super.initState();
-    Future.wait([
-      _getBytesFromAsset(ImageConstants.mapHelp, WidgetSizes.spacingL.toInt()),
-      _getBytesFromAsset(
-        ImageConstants.mapCarHelp,
-        WidgetSizes.spacingL.toInt(),
-      ),
-    ]);
+    asyncInit();
+  }
+
+  Future<void> asyncInit() async {
+    markerHelpIcon = await _getBytesFromAsset(
+        ImageConstants.mapHelp, WidgetSizes.spacingL.toInt());
+    markerCarIcon = await _getBytesFromAsset(
+        ImageConstants.mapCarHelp, WidgetSizes.spacingL.toInt());
+    setState(() {});
   }
 
   @override
@@ -35,41 +40,72 @@ class _CurrentMapViewState extends State<CurrentMapView> with _ByteMapHelper {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: ColorsCustom.sambacus,
-        title: Text(LocaleKeys.login_currentMap.tr()),
-      ),
-      body: GoogleMap(
-        initialCameraPosition: const CameraPosition(
-          target: _defaultLocation,
-          zoom: AppConstants.defaultMapZoom,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [Text(LocaleKeys.login_currentMap.tr()), FilterDropDown()],
         ),
-        polylines: {
-          Polyline(
-            polylineId: PolylineId(
-              '${_defaultLocation.latitude + _defaultLocation.longitude}',
+      ),
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          GoogleMap(
+            initialCameraPosition: const CameraPosition(
+              target: _defaultLocation,
+              zoom: AppConstants.defaultMapZoom,
             ),
-            points: const [
-              _defaultLocation,
-              _defaultLocationIST,
+            polylines: {
+              Polyline(
+                polylineId: PolylineId(
+                  '${_defaultLocation.latitude + _defaultLocation.longitude}',
+                ),
+                points: const [
+                  _defaultLocation,
+                  _defaultLocationIST,
+                ],
+                width: 4,
+                color: ColorsCustom.sambacus,
+              ),
+            },
+            markers: {
+              Marker(
+                markerId: MarkerId('${_defaultLocation.latitude}'),
+                position: _defaultLocation,
+                draggable: true,
+                onDragEnd: (value) {},
+                icon: markerHelpIcon ?? BitmapDescriptor.defaultMarker,
+              ),
+              Marker(
+                markerId: MarkerId('${_defaultLocationIST.latitude}'),
+                position: _defaultLocationIST,
+                onDragEnd: (value) {},
+                icon: markerCarIcon ?? BitmapDescriptor.defaultMarker,
+              ),
+            },
+          ),
+          ToggleButtons(
+            children: [
+              Padding(
+                padding: PagePadding.horizontalLowSymmetric(),
+                child: Text('Option1'),
+              ),
+              Padding(
+                padding: PagePadding.horizontalLowSymmetric(),
+                child: Text('Option2'),
+              ),
             ],
-            width: 4,
-            color: ColorsCustom.sambacus,
+            isSelected: _selectedOptions,
+            onPressed: (int index) {
+              setState(() {
+                for (var i = 0; i < _selectedOptions.length; i++) {
+                  _selectedOptions[i] = i == index;
+                }
+              });
+            },
+            selectedColor: ColorsCustom.white,
+            fillColor: ColorsCustom.sambacus,
+            borderRadius: context.lowBorderRadius,
           ),
-        },
-        markers: {
-          Marker(
-            markerId: MarkerId('${_defaultLocation.latitude}'),
-            position: _defaultLocation,
-            draggable: true,
-            onDragEnd: (value) {},
-            icon: markerHelpIcon ?? BitmapDescriptor.defaultMarker,
-          ),
-          Marker(
-            markerId: MarkerId('${_defaultLocationIST.latitude}'),
-            position: _defaultLocationIST,
-            onDragEnd: (value) {},
-            icon: markerCarIcon ?? BitmapDescriptor.defaultMarker,
-          ),
-        },
+        ],
       ),
     );
   }
@@ -79,11 +115,36 @@ mixin _ByteMapHelper on State<CurrentMapView> {
   BitmapDescriptor? markerHelpIcon;
   BitmapDescriptor? markerCarIcon;
 
-  Future<void> _getBytesFromAsset(String path, int width) async {
+  Future<BitmapDescriptor> _getBytesFromAsset(String path, int width) async {
     final loadedFile = await DefaultAssetBundle.of(context).load(path);
     final bytes = loadedFile.buffer.asUint8List();
-    setState(() {
-      markerHelpIcon = BitmapDescriptor.fromBytes(bytes);
-    });
+
+    return BitmapDescriptor.fromBytes(bytes);
+  }
+}
+
+class FilterDropDown extends StatelessWidget {
+  FilterDropDown({super.key});
+  final filterItems = ['item1', 'item2', 'item3', 'item4'];
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonHideUnderline(
+      child: DropdownButton<String>(
+        dropdownColor: ColorsCustom.sambacus,
+        items: filterItems
+            .map(
+              (e) => DropdownMenuItem(
+                  value: e,
+                  onTap: () {},
+                  child: Text(
+                    e,
+                    style: const TextStyle(color: ColorsCustom.white),
+                  )),
+            )
+            .toList(),
+        onChanged: (value) {},
+        value: filterItems.first,
+      ),
+    );
   }
 }
