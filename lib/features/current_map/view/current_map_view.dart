3b29112bb7b0bfc8fb0c181e12c02f01802b96cf
vb10 <vbacik.10@gmail.com>
2023-02-10 06:34:51 +0300

    aded on custom map view

---
+++ lib/features/current_map/view/current_map_view.dart
@@ -0,0 +1,89 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:sesimiduy/product/init/language/locale_keys.g.dart';
+import 'package:sesimiduy/product/items/colors_custom.dart';
+import 'package:sesimiduy/product/utility/constants/app_constants.dart';
+import 'package:sesimiduy/product/utility/constants/image_constants.dart';
+import 'package:sesimiduy/product/utility/size/index.dart';
+
+class CurrentMapView extends StatefulWidget {
+  const CurrentMapView({super.key});
+
+  @override
+  State<CurrentMapView> createState() => _CurrentMapViewState();
+}
+
+class _CurrentMapViewState extends State<CurrentMapView> with _ByteMapHelper {
+  static const _defaultLocation = LatLng(37.579609, 36.946812);
+  static const _defaultLocationIST = LatLng(40.5333232, 31.0325468);
+
+  @override
+  void initState() {
+    super.initState();
+    Future.wait([
+      _getBytesFromAsset(ImageConstants.mapHelp, WidgetSizes.spacingL.toInt()),
+      _getBytesFromAsset(
+        ImageConstants.mapCarHelp,
+        WidgetSizes.spacingL.toInt(),
+      ),
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: ColorsCustom.sambacus,
+        title: Text(LocaleKeys.login_currentMap.tr()),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
+          target: _defaultLocation,
+          zoom: AppConstants.defaultMapZoom,
+        ),
+        polylines: {
+          Polyline(
+            polylineId: PolylineId(
+              '${_defaultLocation.latitude + _defaultLocation.longitude}',
+            ),
+            points: const [
+              _defaultLocation,
+              _defaultLocationIST,
+            ],
+            width: 4,
+            color: ColorsCustom.sambacus,
+          ),
+        },
+        markers: {
+          Marker(
+            markerId: MarkerId('${_defaultLocation.latitude}'),
+            position: _defaultLocation,
+            draggable: true,
+            onDragEnd: (value) {},
+            icon: markerHelpIcon ?? BitmapDescriptor.defaultMarker,
+          ),
+          Marker(
+            markerId: MarkerId('${_defaultLocationIST.latitude}'),
+            position: _defaultLocationIST,
+            onDragEnd: (value) {},
+            icon: markerCarIcon ?? BitmapDescriptor.defaultMarker,
+          ),
+        },
+      ),
+    );
+  }
+}
+
+mixin _ByteMapHelper on State<CurrentMapView> {
+  BitmapDescriptor? markerHelpIcon;
+  BitmapDescriptor? markerCarIcon;
+
+  Future<void> _getBytesFromAsset(String path, int width) async {
+    final loadedFile = await DefaultAssetBundle.of(context).load(path);
+    final bytes = loadedFile.buffer.asUint8List();
+    setState(() {
+      markerHelpIcon = BitmapDescriptor.fromBytes(bytes);
+    });
+  }
+}
--- lib/features/login/view/login_view.dart
+++ lib/features/login/view/login_view.dart
@@ -1,9 +1,11 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:kartal/kartal.dart';
 import 'package:sesimiduy/core/enums/core_locale.dart';
+import 'package:sesimiduy/features/current_map/view/current_map_view.dart';
 import 'package:sesimiduy/product/dialog/request_help_dialog.dart';
 import 'package:sesimiduy/product/init/language/locale_keys.g.dart';
 import 'package:sesimiduy/product/items/colors_custom.dart';
@@ -97,7 +99,9 @@ class _CurrentMaps extends StatelessWidget {
           ColorsCustom.endless,
         ),
       ),
-      onPressed: () {},
+      onPressed: () {
+        context.navigateToPage<void>(const CurrentMapView());
+      },
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -164,48 +168,51 @@ class _Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: WidgetSizes.spacingXxlL14,
-      child: Stack(
-        children: [
-          ColoredBox(
-            color: ColorsCustom.sambacus,
-            child: Padding(
-              padding: const PagePadding.horizontalSymmetric() +
-                  const PagePadding.onlyTopNormal(),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    ImageConstants.logo,
-                    height: IconSize.large.value,
-                  ),
-                  const SizedBox(height: WidgetSizes.spacingXsMid),
-                  Text(
-                    LocaleKeys.login_info.tr(),
-                    textAlign: TextAlign.center,
-                    style: context.textTheme.titleLarge?.copyWith(
-                      color: ColorsCustom.white,
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle.light,
+      child: SizedBox(
+        height: WidgetSizes.spacingXxlL14,
+        child: Stack(
+          children: [
+            ColoredBox(
+              color: ColorsCustom.sambacus,
+              child: Padding(
+                padding: const PagePadding.horizontalSymmetric() +
+                    const PagePadding.onlyTopNormal(),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      ImageConstants.logo,
+                      height: IconSize.large.value,
                     ),
-                  ),
-                ],
+                    const SizedBox(height: WidgetSizes.spacingXsMid),
+                    Text(
+                      LocaleKeys.login_info.tr(),
+                      textAlign: TextAlign.center,
+                      style: context.textTheme.titleLarge?.copyWith(
+                        color: ColorsCustom.white,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: context.dynamicWidth(.3),
-            child: const _ClipperTriangle(),
-          ),
-          Positioned(
-            right: 0,
-            child: SafeArea(
-              child: LanguageDropDown(
-                value: CoreLocale.fromLocale(context.locale).changeCrossFlag,
+            Positioned(
+              bottom: 0,
+              left: context.dynamicWidth(.3),
+              child: const _ClipperTriangle(),
+            ),
+            Positioned(
+              right: 0,
+              child: SafeArea(
+                child: LanguageDropDown(
+                  value: CoreLocale.fromLocale(context.locale).changeCrossFlag,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
