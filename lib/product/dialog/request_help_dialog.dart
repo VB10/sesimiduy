0e6252f03902108f69a1abb4cec68279fcd4e666
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2023-02-10 03:56:29 +0300

    add all

--- lib/product/dialog/request_help_dialog.dart
+++ lib/product/dialog/request_help_dialog.dart
@@ -1,43 +1,158 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:kartal/kartal.dart';
+import 'package:sesimiduy/product/enum/widget_size.dart';
+import 'package:sesimiduy/product/init/language/locale_keys.g.dart';
+import 'package:sesimiduy/product/items/colors_custom.dart';
+import 'package:sesimiduy/product/model/needs_model.dart';
 import 'package:sesimiduy/product/utility/constants/app_constants.dart';
+import 'package:sesimiduy/product/utility/constants/string_constants.dart';
 import 'package:sesimiduy/product/utility/padding/page_padding.dart';
+import 'package:sesimiduy/product/utility/validator/validator_items.dart';
+import 'package:sesimiduy/product/widget/button/active_button.dart';
+import 'package:sesimiduy/product/widget/combo_box/labeled_product_combo_box.dart';
 import 'package:sesimiduy/product/widget/text_field/labeled_product_textfield.dart';
 
 class RequestHelpDialog extends StatelessWidget {
-  const RequestHelpDialog({super.key});
+  RequestHelpDialog({super.key});
+  final GlobalKey<FormState> _formKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
       insetPadding: const PagePadding.all(),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          context.emptySizedHeightBoxLow,
-          const _Header(),
-          const _CustomDivider(),
-          context.emptySizedHeightBoxLow3x,
-          const Padding(
-            padding: PagePadding.horizontalSymmetric(),
-            child: Text('Yardıma ihtiyacı olan kişinin'),
+      child: SingleChildScrollView(
+        child: Form(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              context.emptySizedHeightBoxLow,
+              const _Header(),
+              const _CustomDivider(),
+              context.emptySizedHeightBoxLow3x,
+              const _SubHeader(),
+              context.emptySizedHeightBoxLow3x,
+              const _FullNameField(),
+              context.emptySizedHeightBoxLow3x,
+              const _PhoneNumberField(),
+              context.emptySizedHeightBoxLow3x,
+              const _AddressField(),
+              context.emptySizedHeightBoxLow3x,
+              const _NeedsComboBox(),
+              context.emptySizedHeightBoxLow3x,
+              const _CustomDivider(),
+              context.emptySizedHeightBoxLow3x,
+              const _ActionButton(),
+              context.emptySizedHeightBoxLow3x,
+            ],
           ),
-          context.emptySizedHeightBoxLow3x,
-          const Padding(
-            padding: PagePadding.horizontalSymmetric(),
-            child: LabeledProductTextField(labelText: 'Adı Soyadı'),
-          ),
-          context.emptySizedHeightBoxLow3x,
-          const Padding(
-            padding: PagePadding.horizontalSymmetric(),
-            child: LabeledProductTextField(labelText: 'Telefon Numarası'),
-          ),
-          context.emptySizedHeightBoxLow3x,
-          const Padding(
-            padding: PagePadding.horizontalSymmetric(),
-            child: LabeledProductTextField(labelText: 'Adresi'),
-          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ActionButton extends StatelessWidget {
+  const _ActionButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const PagePadding.horizontalSymmetric(),
+      child: ActiveButton(
+        onPressed: () {},
+        label: LocaleKeys.askForHelp.tr(),
+      ),
+    );
+  }
+}
+
+class _NeedsComboBox extends StatelessWidget {
+  const _NeedsComboBox();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const PagePadding.horizontalSymmetric(),
+      child: LabeledProductComboBox<NeedsModel>(
+        validator: (item) => ValidatoGenericItems<NeedsModel>(item).validateDropDown,
+        hintText: 'Çoklu Seçim Yapabilirsiniz',
+        onChanged: (value) {},
+        labelText: LocaleKeys.hintNameNeed.tr(),
+        items: [
+          NeedsModel('option 1'),
+          NeedsModel('option 1'),
+          NeedsModel('option 1'),
+          NeedsModel('option 1'),
+          NeedsModel('option 1'),
+          NeedsModel('option 1'),
         ],
+      ),
+    );
+  }
+}
+
+class _AddressField extends StatelessWidget {
+  const _AddressField();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const PagePadding.horizontalSymmetric(),
+      child: LabeledProductTextField(
+        hintText: LocaleKeys.addressHint.tr(),
+        labelText: LocaleKeys.address.tr(),
+        isMultiline: true,
+        validator: (value) => ValidatorItems(value).validateAdress,
+      ),
+    );
+  }
+}
+
+class _PhoneNumberField extends StatelessWidget {
+  const _PhoneNumberField();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const PagePadding.horizontalSymmetric(),
+      child: LabeledProductTextField(
+        hintText: StringConstants.phoneHint,
+        formatters: [InputFormatter.instance.phoneFormatter],
+        validator: (value) => ValidatorItems(value).validatePhoneNumber,
+        labelText: LocaleKeys.phoneNumber.tr(),
+      ),
+    );
+  }
+}
+
+class _FullNameField extends StatelessWidget {
+  const _FullNameField();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const PagePadding.horizontalSymmetric(),
+      child: LabeledProductTextField(
+        hintText: LocaleKeys.nameAndSurname.tr(),
+        labelText: LocaleKeys.nameAndSurname.tr(),
+        validator: (value) => ValidatorItems(value).validateFullName,
+      ),
+    );
+  }
+}
+
+class _SubHeader extends StatelessWidget {
+  const _SubHeader();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const PagePadding.horizontalSymmetric(),
+      child: Text(
+        LocaleKeys.personNeedsHelp.tr(),
       ),
     );
   }
@@ -49,6 +164,8 @@ class _CustomDivider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Divider(
+      color: ColorsCustom.lightGray,
+      thickness: AppConstants.kOne.toDouble(),
       height: AppConstants.kZero.toDouble(),
     );
   }
@@ -64,8 +181,10 @@ class _Header extends StatelessWidget {
       child: Row(
         children: [
           Text(
-            'YARDIM İSTİYORUM',
-            style: context.textTheme.headlineSmall,
+            LocaleKeys.needHelp.tr(),
+            style: context.textTheme.titleLarge?.copyWith(
+              fontWeight: FontWeight.w600,
+            ),
           ),
           const Spacer(),
           IconButton(
@@ -73,6 +192,7 @@ class _Header extends StatelessWidget {
               context.navigation.pop();
             },
             icon: const Icon(Icons.close),
+            iconSize: WidgetSizes.spacingXxl,
           )
         ],
       ),
--- lib/product/items/colors_custom.dart
+++ lib/product/items/colors_custom.dart
@@ -5,4 +5,6 @@ class ColorsCustom {
   static const Color sambacus = Color(0xFF101A2A);
   static const Color endless = Color(0xFFE23E3E);
   static const Color braziliante = Color(0xFF34C759);
+  static const Color brandeisBlue = Color(0xFF0D6EFD);
+  static const Color lightGray = Color(0xffCED4DA);
 }
