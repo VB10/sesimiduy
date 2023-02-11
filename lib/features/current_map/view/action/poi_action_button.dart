228d1339e2b6df73caf7ec5647d76d7e1a7083ac
Veli Bacik <vbacik.10@gmail.com>
2023-02-11 19:35:14 +0300

    added on lateset changes

---
+++ lib/features/current_map/view/action/poi_action_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter_platform_interface/src/types/bitmap.dart';
+import 'package:sesimiduy/features/current_map/view/current_map_view.dart';
+import 'package:sesimiduy/features/current_map/view/dropdown/filter_dropdown.dart';
+import 'package:sesimiduy/features/login/service/map_service.dart';
+
+class PoiActionButton extends StatelessWidget {
+  const PoiActionButton({
+    required this.onSelected,
+    super.key,
+    this.icon,
+  });
+  final AsyncValueSetter<Set<ProductMarker>> onSelected;
+  final BitmapDescriptor? icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return FilterDropDown(
+      onSelected: (category) async {
+        if (category == null) return;
+        final markers = await MapService().fetchPOI(category);
+        await onSelected.call(
+          markers
+              .map(
+                (e) => ProductMarker(
+                  e.id,
+                  e.name,
+                  e.location?.latitude,
+                  e.location?.longitude,
+                  icon: icon,
+                ),
+              )
+              .toSet(),
+        );
+      },
+    );
+  }
+}
