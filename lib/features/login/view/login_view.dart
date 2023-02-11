78841481e9f193d18e0db079a229470808c55d90
kagangullu <oguzkagangullu@gmail.com>
2023-02-11 19:39:36 +0300

    add snackbar

--- lib/features/login/view/login_view.dart
+++ lib/features/login/view/login_view.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -236,11 +238,23 @@ class _HelpWantedButtonState extends State<_HelpWantedButton> {
 
     if (response == null) {
       _changeLoading();
+      showInSnackBar(LocaleKeys.requestFailed.tr(), context);
       return;
     }
     final uploadService = HelpUploadService();
     await uploadService.createHelpCall(helpForm: response);
     _changeLoading();
+  }
+
+  void showInSnackBar(String title, BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(title),
+        backgroundColor: ColorsCustom.sambacus,
+        showCloseIcon: true,
+        closeIconColor: Colors.white,
+      ),
+    );
   }
 }
 
