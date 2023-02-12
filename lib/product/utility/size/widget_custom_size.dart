294c9b5aac79d28647f830167e911f516d5ad4e4
Veli Bacik <vbacik.10@gmail.com>
2023-02-13 00:24:04 +0300

    added latest changes for ui fixing

---
+++ lib/product/utility/size/widget_custom_size.dart
@@ -0,0 +1,11 @@
+enum WidgetCustomSize {
+  //size is [300]
+  header(300),
+
+  //size is [300]
+  headerIcon(200);
+
+  final double value;
+  // ignore: sort_constructors_first
+  const WidgetCustomSize(this.value);
+}
