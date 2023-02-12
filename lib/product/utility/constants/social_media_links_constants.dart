373298afcafac98207ea921301e428e11ba380ee
Beyza Karadeniz <beyza.karadeniz@lcwaikiki.com>
2023-02-12 23:57:05 +0300

    home view feedback fixed

--- lib/product/utility/constants/social_media_links_constants.dart
+++ lib/product/utility/constants/social_media_links_constants.dart
@@ -1,5 +1,5 @@
 class SocialMediaLinksConstants {
-  static const String twitterUrl = '';
-  static const String instagramUrl = '';
-  static const String afadUrl = '';
+  static const String twitterUrl = 'https://twitter.com/afadbaskanlik';
+  static const String instagramUrl = 'https://www.instagram.com/afadbaskanlik';
+  static const String afadUrl = 'https://www.afad.gov.tr';
 }
